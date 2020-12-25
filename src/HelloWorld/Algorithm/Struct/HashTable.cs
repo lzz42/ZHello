064f52c22e5bec2f62ac35b8eb2964cecca14104
lzz42 <hbuzlz@163.com>
2020-12-25 17:53:44 +0800

    新增二叉树遍历算法

---
+++ src/HelloWorld/Algorithm/Struct/HashTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZHello.Algorithm.Struct
+{
+
+    /// <summary>
+    /// 11.1 直接寻址表
+    /// 关键字k的范围比较小，次数直接使用关键字作为下标
+    /// </summary>
+    public class DirectAddressTable<K>
+    {
+        /// <summary>
+        /// 全域U
+        /// </summary>
+        private K[] U { get; set; }
+
+        private K this[int i]
+        {
+            get { return U[i]; }
+            set { U[i] = value; }
+        }
+
+        public K Search(int k)
+        {
+            return U[k];
+        }
+
+        public void Insert(K k,int x)
+        {
+            U[x] = k;
+        }
+
+        public void Delete(int x)
+        {
+            U[x] = default(K);
+        }
+    }
+
+    public class HashTable
+    {
+        public static int DevideHash(int k,int m)
+        {
+            var ret = 0;
+            ret = k % m;
+            return ret;
+        }
+
+        public static int MuiltiHash(int k,int m)
+        {
+            //A=(sqrt(5)-2)/2 =0.618 033 988 7
+            float A = 0.5f;
+            var ret = 0;
+            ret = (int)Math.Floor(m * (k * m - Math.Floor(k * A)));
+            return ret;
+        }
+    }
+}
--- src/HelloWorld/Algorithm/Struct/Tree.cs
+++ src/HelloWorld/Algorithm/Struct/Tree.cs
@@ -1,182 +1,241 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZHello.Algorithm.Struct
 {
-    public class TNode<K>
+    /// <summary>
+    /// 二叉树
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    public class BTree<K>
         where K : IComparable
     {
-        public static TNode<K> NIL = null;
         public K Key { get; set; }
-        public TNode<K> P { get; set; }
+        public BTree<K> Left { get; set; }
+        public BTree<K> Right { get; set; }
     }
 
-    public class BTNode<K> : TNode<K>
+    /// <summary>
+    /// 左孩子右兄弟的有根树
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    public class NTree<K>
         where K : IComparable
     {
-        public BTNode<K> Left { get; set; }
-        public BTNode<K> Right { get; set; }
-    }
+        public K Key { get; set; }
 
-    public class BTree<N, K>
-        where K : IComparable
-        where N : BTNode<K>
-    {
-        public N NIL = null;
-
-        public BTree()
-        {
-            Root = NIL;
-        }
-
-        public N Root { get; set; }
-
-        public bool IsEmpty()
-        {
-            return Root == NIL;
-        }
-
-        public bool IsRoot(N x)
-        {
-            return x.P == NIL;
-        }
-    }
-
-    public class NTNode<K> : TNode<K>
-        where K : IComparable
-    {
         /// <summary>
         /// 节点x左侧孩子节点,无孩子节点为NIL
         /// </summary>
-        public NTNode<K> LeftChild { get; set; }
+        public NTree<K> LeftChild { get; set; }
 
         /// <summary>
         /// 节点x右侧兄弟节点,无右侧兄弟节点为NIL
         /// </summary>
-        public NTNode<K> RightSibling { get; set; }
-    }
-
-    public class NTree<N, K>
-        where K : IComparable
-        where N : NTNode<K>
-    {
-        public static N NIL = null;
-        public N Root { get; set; }
+        public NTree<K> RightSibling { get; set; }
     }
 
     public static class P10_4
     {
         /// <summary>
+        /// 遍历顺序
+        /// </summary>
+        public enum TraverseOrder
+        {
+            /// <summary>
+            /// 先序遍历 根-左子-右子
+            /// </summary>
+            PreOrder = 1,
+
+            /// <summary>
+            /// 中序遍历 左子-根-右子
+            /// </summary>
+            InOrder = 2,
+
+            /// <summary>
+            /// 后序遍历 左子-右子-根
+            /// </summary>
+            PostOrder = 3,
+
+            /// <summary>
+            /// 层级优先遍历
+            /// </summary>
+            LevelOrder = 4,
+        }
+
+        /// <summary>
         /// 10.4.2
+        /// n结点的二叉树，使用O(n)的递归过程，遍历所有的节点的关键字
         /// </summary>
         /// <typeparam name="N"></typeparam>
         /// <typeparam name="K"></typeparam>
         /// <param name="tree"></param>
         /// <param name="node"></param>
-        public static void PrintBTree<N, K>(BTree<N, K> tree, BTNode<K> node = null)
+        public static void PrintBTree<K>(BTree<K> tree, TraverseOrder order)
             where K : IComparable
-            where N : BTNode<K>
         {
-            if (node == null)
+            if (tree == null)
+                return;
+            switch (order)
             {
-                if (tree == null)
-                    return;
-                if (tree.IsEmpty())
-                    return;
-                PrintBTree(tree, tree.Root);
-            }
-            else
-            {
-                Trace.WriteLine(node.Key);
-                if (node.Left != null)
-                    PrintBTree(tree, node.Left);
-                if (node.Right != null)
-                    PrintBTree(tree, node.Right);
+                case TraverseOrder.PreOrder:
+                    Trace.WriteLine(tree.Key);
+                    PrintBTree(tree.Left, order);
+                    PrintBTree(tree.Right, order);
+                    break;
+
+                case TraverseOrder.InOrder:
+                    PrintBTree(tree.Left, order);
+                    Trace.WriteLine(tree.Key);
+                    PrintBTree(tree.Right, order);
+                    break;
+
+                case TraverseOrder.PostOrder:
+                    PrintBTree(tree.Left, order);
+                    PrintBTree(tree.Right, order);
+                    Trace.WriteLine(tree.Key);
+                    break;
+
+                case TraverseOrder.LevelOrder:
+                    break;
+
+                default:
+                    break;
             }
         }
 
         /// <summary>
         /// 10.4.3
+        /// n结点的二叉树，使用O(n)的非递归过程，遍历所有的节点的关键字
         /// </summary>
         /// <typeparam name="N"></typeparam>
         /// <typeparam name="K"></typeparam>
         /// <param name="tree"></param>
         /// <param name="node"></param>
-        public static void PrintBTree<N, K>(BTree<N, K> tree)
+        public static void PrintBTree<N, K>(BTree<K> tree, TraverseOrder order)
             where K : IComparable
-            where N : BTNode<K>
         {
             if (tree == null)
                 return;
-            if (tree.IsEmpty())
-                return;
-            var node = (BTNode<K>)tree.Root;
-            var stack = new Stack<BTNode<K>>(1024);
-            while (node != null || !stack.IsEmpty())
+            var stack = new Stack<BTree<K>>(1024);
+            switch (order)
             {
-                while (node != null)
-                {
-                    Trace.WriteLine(node.Key);
-                    stack.Push(node);
-                    node = node.Left;
-                }
-                if (!stack.IsEmpty())
-                {
-                    node = stack.Pop();
-                    node = node.Right;
-                }
+                case TraverseOrder.PreOrder:
+                    //前序遍历
+                    while (tree != null || !stack.IsEmpty())
+                    {
+                        while (tree != null)
+                        {
+                            Trace.WriteLine(tree.Key);
+                            stack.Push(tree);
+                            tree = tree.Left;
+                        }
+                        if (!stack.IsEmpty())
+                        {
+                            tree = stack.Pop();
+                            tree = tree.Right;
+                        }
+                    }
+                    break;
+                case TraverseOrder.InOrder:
+                    //中序遍历
+                    while (tree != null || !stack.IsEmpty())
+                    {
+                        while (tree != null)
+                        {
+                            stack.Push(tree);
+                            tree = tree.Left;
+                        }
+                        if (!stack.IsEmpty())
+                        {
+                            tree = stack.Pop();
+                            Trace.WriteLine(tree.Key);
+                            tree = tree.Right;
+                        }
+                    }
+                    break;
+                case TraverseOrder.PostOrder:
+                    //后序遍历
+                    var stack2 = new Stack<BTree<K>>(1024);
+                    while (tree != null || !stack.IsEmpty())
+                    {
+                        while (tree != null)
+                        {
+                            stack.Push(tree);
+                            stack2.Push(tree);
+                            tree = tree.Right;
+                        }
+                        if (!stack.IsEmpty())
+                        {
+                            tree = stack.Pop();
+                            tree = tree.Left;
+                        }
+                    }
+                    while (!stack2.IsEmpty())
+                    {
+                        tree = stack2.Pop();
+                        Trace.WriteLine(tree.Key);
+                    }
+                    break;
+                case TraverseOrder.LevelOrder:
+                    break;
+                default:
+                    break;
             }
         }
 
         /// <summary>
         /// 10.4.4
+        /// n结点的任意有根树，使用O(n)的算法，遍历所有的节点的关键字
+        /// 该树以左孩子右兄弟表示
         /// </summary>
         /// <typeparam name="N"></typeparam>
         /// <typeparam name="K"></typeparam>
         /// <param name="tree"></param>
         /// <param name="node"></param>
-        public static void PrintNTree<N, K>(NTree<N, K> tree, NTNode<K> node = null)
+        public static void PrintNTree<K>(NTree<K> tree, TraverseOrder order)
             where K : IComparable
-            where N : NTNode<K>
         {
-            if (node == null)
+            if (tree == null)
+                return;
+            switch (order)
             {
-                if (tree == null)
-                    return;
-                PrintNTree(tree, tree.Root);
-            }
-            else
-            {
-                Trace.WriteLine(node.Key);
-                if (node.LeftChild != null)
-                    PrintNTree(tree, node.LeftChild);
-                if (node.RightSibling != null)
-                    PrintNTree(tree, node.RightSibling);
+                case TraverseOrder.PreOrder:
+                    Trace.WriteLine(tree.Key);
+                    PrintNTree(tree.LeftChild, order);
+                    PrintNTree(tree.RightSibling, order);
+                    break;
+                case TraverseOrder.InOrder:
+                    PrintNTree(tree.LeftChild, order);
+                    Trace.WriteLine(tree.Key);
+                    PrintNTree(tree.RightSibling, order);
+                    break;
+                case TraverseOrder.PostOrder:
+                    PrintNTree(tree.LeftChild, order);
+                    PrintNTree(tree.RightSibling, order);
+                    Trace.WriteLine(tree.Key);
+                    break;
+                case TraverseOrder.LevelOrder:
+                    break;
+                default:
+                    break;
             }
         }
 
         /// <summary>
         /// 10.4.5
+        /// n结点的二叉树，使用O(n)的非递归算法，遍历所有的节点的关键字
+        /// 要求：除树本身存储空间外只能使用固定量的额外存储空间，且不能修改树
         /// </summary>
         /// <typeparam name="N"></typeparam>
         /// <typeparam name="K"></typeparam>
         /// <param name="tree"></param>
         /// <param name="node"></param>
-        public static void PrintBTree5<N, K>(BTree<N, K> tree)
+        public static void PrintBTree5<K>(BTree<K> tree)
             where K : IComparable
-            where N : BTNode<K>
         {
             if (tree == null)
                 return;
-            if (tree.IsEmpty())
-                return;
-            var node = tree.Root;
-            //
         }
-
     }
 }
